e78a51cb83b27fe6b4da57bfff41f8b4391ba39d
huamurui <60086749+huamurui@users.noreply.github.com>
2021-07-18 10:46:45 +0800

    就只是复制

---
+++ MyShelter/Companion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShelter
+{
+    public class Companion
+    {
+        public string UID { get; }
+        public string Owner{ get; set; }
+        public string  email{ get; set; }
+        public string password { get; set; }
+
+
+        public string Number { get; }
+        private static int companionNumberSeed = 0;
+        public Companion(string name,string Email)
+        {
+            this.Owner = name;
+            this.email = Email;
+            this.Number = companionNumberSeed.ToString();
+            companionNumberSeed++;
+        }
+        public void FaQ(DateTime data,string note) { }
+    }
+}
---
+++ MyShelter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//从0.000001开始的OOP
+//说起来，写这个。。。好像跟小时候玩的那些桌面游戏好像。。。还像写翻译题
+namespace MyShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var companion = new Companion("hya", "hyq@m.com");
+            Console.WriteLine($"Companion {companion.UID} was created for {companion.Owner} with email @{companion.email}.");
+        }
+    }
+}
